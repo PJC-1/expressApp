13a4da4732179d2620e771169e567d35316ffb0a
PJC-1 <jumpzine@gmail.com>
2017-10-29 21:46:51 -0700

    added nav and get route for all todos

--- server.js
+++ server.js
@@ -9,9 +9,25 @@ app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended : true }));
 
+////////////////////
+// HTML Endpoints //
+////////////////////
+
+// GET Route for Root
 app.get('/', function(req, res){
-  res.sendFile('views/index.html', {root : __dirname})
+  res.sendFile('views/index.html', {root : __dirname});
 });
+
+// GET Route for all todos
+app.get('/todos', function(req, res){
+  res.sendFile('views/allTodos.html', {root : __dirname});
+});
+
+///////////////////
+// API Endpoints //
+///////////////////
+
+
 
 app.listen(3000, function(){
   console.log("now listening to port 3000...");

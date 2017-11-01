c824345ad1c980a7696dc8331e28a1054b434c31
PJC-1 <jumpzine@gmail.com>
2017-10-31 22:18:23 -0700

    showing a single todo SUCCESS

---
+++ public/scripts/todo.js
@@ -0,0 +1,35 @@
+console.log("testing todo.js...");
+
+$(document).ready(function(){
+  var windowPath = window.location.pathname;
+  var windowPathSplit = windowPath.split('/');
+  var id = windowPathSplit[2];
+  var url = '/api/todos/' + id;
+
+  $.ajax({
+    method: "GET",
+    url: url,
+    success: onSuccess,
+    error: onError
+  });
+});
+
+function onSuccess(json){
+  console.log(json);
+  var output = `
+    <p>
+      <strong>author: </strong> ${json.author}
+    </p>
+    <p>
+      <strong>description: </strong> ${json.description}
+    </p>
+    <p>
+      <strong>difficulty: </strong> ${json.difficutlyLevel}
+    </p>
+  `;
+  $('#todoTarget').append(output);
+}
+
+function onError(error){
+  console.log(error);
+}
